7a315b39943bfb8abfce154def21e71ddce5c105
Abhishek Pandit <abhishekpanditoff@gmail.com>
2022-11-16 15:02:37 +0530

    changes

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -2,41 +2,17 @@ const httpStatus = require('http-status');
 
 const { catchAsync } = require('../utils');
 const { TestServices } = require('../services');
-const { s3Uploader } = require("../utils/cloud-provider/modules/aws");
+
 
 const createData = catchAsync(async(req, res, next) => {
-    var file = req.file;
-    var path = req.body.filePath;
-
-    uploadParams = {
-        Bucket: "newsarticle-image-upload",
-        Body: file.buffer,
-        Key: path,
-        ContentType: file.mimetype
-    };
-
-    await putObjectS3(uploadParams, next, async function(data) {
-        req.body.s3_image = data.location;
         const doc = await TestServices.createData(req.body, next);
         res.status(httpStatus.CREATED).json({
             status: "Success",
             message: "Created Successfully",
             doc
         })
-    });
 })
 
-
-const putObjectS3 = async(uploadParams, next, callback) => {
-    await s3Uploader.upload(uploadParams, next, async function(data) {
-        const fileRecord = {
-            bucket_name: uploadParams.Bucket,
-            key: data.Key,
-            location: data.Location,
-        };
-        callback(fileRecord);
-    });
-};
 
 const getData = catchAsync(async(req, res, next) => {
     const data = await TestServices.getData({ _id: req.params.id }, next);
